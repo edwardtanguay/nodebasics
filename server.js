d108f47d5bc60ca80662201c461f9cc60d8420ce
Edward Tanguay (wk) <edward@tanguay.info>
2020-08-06 14:09:23 +0200

    form works

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
 const app = express();
-
+const port = 3001;
 
 app.use(express.static(__dirname));
-app.listen(3000);
+const server = app.listen(port, () => {
+	console.log(`Listening on port ${server.address().port}...`);
+}); 
